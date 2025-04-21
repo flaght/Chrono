f69698c30125ed547019bc38f9712c6fa0c2c559
Kerry <flaght@gmail.com>
2025-04-21 23:40:32 +0000

    删除冗余代码

--- entangle/macro/contract.py
+++
@@ -1,8 +0,0 @@
-import toml, os
-
-contract_file = os.path.join('config/contract.toml')
-contract = toml.load(contract_file)
-
-MAIN_CONTRACT_MAPPING = contract['MAIN_CONTRACT']
-CONT_MULTNUM_MAPPING = contract['CONT_MULTNUM']
-SYMBOL_CONTRANCT_MAPPING = contract['SYMBOL_CONTRANCT']
