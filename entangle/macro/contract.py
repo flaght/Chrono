a004678d4a0fd828a2699eb8547a69946bf7fe80
Kerry <flaght@gmail.com>
2025-04-21 07:20:19 +0000

    add new symbol

--- entangle/macro/contract.py
+++ entangle/macro/contract.py
@@ -5,3 +5,4 @@ contract = toml.load(contract_file)
 
 MAIN_CONTRACT_MAPPING = contract['MAIN_CONTRACT']
 CONT_MULTNUM_MAPPING = contract['CONT_MULTNUM']
+SYMBOL_CONTRANCT_MAPPING = contract['SYMBOL_CONTRANCT']
