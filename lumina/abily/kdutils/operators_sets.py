9407dc3ebb7c4545f9a0710dccc58844b1ac774d
Kerry <flaght@gmail.com>
2025-04-28 03:22:47 +0000

    调整算子

--- lumina/abily/kdutils/operators_sets.py
+++ lumina/abily/kdutils/operators_sets.py
@@ -1,19 +1,18 @@
 two_operators_sets = [
-    'MConVariance', 'MMASSI', 'MACCBands', 'MPWMA', 'MIChimoku', 'MRes',
-    'MMeanRes', 'MCORR', 'MCoef', 'MSLMean', 'MSmart', 'MSharp', 'MSortino',
-    'MINIMUM', 'MAXIMUM', 'ADDED', 'SUBBED', 'MUL', 'DIV', 'MOD'
+    'MConVariance', 'MMASSI', 'MACCBands', 'MIChimoku', 'MRes', 'MMeanRes',
+    'MCORR', 'MCoef', 'MSLMean', 'MSmart', 'MSharp', 'MSortino', 'MINIMUM',
+    'MAXIMUM', 'ADDED', 'SUBBED', 'MUL', 'DIV', 'MOD'
 ]
 #, 'MHMA', 'MDPO', 'MARETURNLog',
 one_operators_sets = [
     'MA', 'MPERCENT', 'MMedian', 'MADiff', 'MADecay', 'MMAX', 'MMIN', 'MDPO',
     'MARGMAX', 'MARGMIN', 'MRANK', 'MQUANTILE', 'MCPS', 'MDIFF', 'MMaxDiff',
     'MMinDiff', 'MSUM', 'MPRO', 'MVARIANCE', 'MVHF', 'MDPO', 'MT3', 'MDEMA',
-    'MIR', 'MSKEW', 'MKURT', 'MSTD', 'MNPOSITIVE', 'MAPOSITIVE', 'EMA', 'RSI',
-    'WMA', 'MMaxDrawdown', 'MMDrawdown', 'SIGN', 'AVG', 'SQRT', 'DIFF', 'LOG2',
-    'LOG10', 'LOG', 'EXP', 'FRAC', 'SIGLOG2ABS', 'SIGLOG10ABS', 'SIGLOGABS',
-    'POW', 'ABS', 'ACOS', 'ASIN', 'NORMINV', 'CEIL', 'FLOOR', 'ROUND', 'TANH',
-    'RELU', 'SHIFT', 'DELTA', 'SIGMOID', 'RETURNSimple', 'RETURNLog'
+    'MIR', 'MSKEW', 'MKURT', 'MSTD', 'EMA', 'RSI', 'WMA', 'MMaxDrawdown',
+    'MMDrawdown', 'SIGN', 'AVG', 'SQRT', 'DIFF', 'LOG2', 'LOG10', 'LOG', 'EXP',
+    'FRAC', 'SIGLOG2ABS', 'SIGLOG10ABS', 'SIGLOGABS', 'POW', 'ABS', 'ACOS',
+    'ASIN', 'NORMINV', 'CEIL', 'FLOOR', 'ROUND', 'TANH', 'RELU', 'SHIFT',
+    'DELTA', 'SIGMOID'
 ]
 
 operators_sets = two_operators_sets + one_operators_sets
-
