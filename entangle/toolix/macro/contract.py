9061caf951a66362929314a33c1c5b1b8da36269
Kerry <flaght@gmail.com>
2025-04-28 03:25:11 +0000

    新增策略配置项

--- entangle/toolix/macro/contract.py
+++ entangle/toolix/macro/contract.py
@@ -2,6 +2,12 @@ import toml, os, pdb
 contract_file = os.path.join(os.environ['ENTL_CONFIG'], 'config/contract.toml')
 contract = toml.load(contract_file)
 
+### 基础合约配置
 MAIN_CONTRACT_MAPPING = contract['MAIN_CONTRACT']
 CONT_MULTNUM_MAPPING = contract['CONT_MULTNUM']
 SYMBOL_CONTRANCT_MAPPING = contract['SYMBOL_CONTRANCT']
+
+
+### 策略配置
+CHAOS_PHECDA_MAPPING = contract['CHAOS_PHECDA']
+
