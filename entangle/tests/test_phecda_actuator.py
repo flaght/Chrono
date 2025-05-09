e88bf116d9c3d60f6fc6a1fac2fdf457d9984e5b
Kerry <flaght@gmail.com>
2025-05-09 02:35:46 +0000

    修改例子

---
+++ entangle/tests/test_phecda_actuator.py
@@ -0,0 +1,32 @@
+import os, sys, datetime, pdb
+import pandas as pd
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+sys.path.insert(0, os.path.abspath('../'))
+
+from plugin.phecda.signalor import Signalor
+
+
+def create_time(begin_time, end_time, freq='1T'):
+    """
+    Create a time range with the specified frequency.
+    """
+    return pd.date_range(start=begin_time, end=end_time, freq=freq)
+
+
+def main():
+    #symbol = 'rb2510'
+    code = 'IM'
+    trade_time = pd.to_datetime('2025-04-23 10:50:00')
+    begin_time = pd.to_datetime('2025-04-23 10:35:00')
+    trade_times = create_time(begin_time, trade_time, freq='1T')
+    actuator = Signalor(code=code, id='1078761302')
+    for trade_time in trade_times:
+        pdb.set_trace()
+        actuator.run(trade_time=pd.to_datetime(trade_time.strftime('%Y-%m-%d %H:%M:%S')))
+
+
+main()
