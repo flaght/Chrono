79c45df52fba68de332e21e0bd50922395ca2c3d
Kerry <flaght@gmail.com>
2025-05-16 15:08:39 +0000

    Adjust the startup parameters

--- kichaos/phecda/2.1.1.long_sac_base.py
+++ kichaos/phecda/2.1.1.long_sac_base.py
@@ -75,43 +75,109 @@ def fit(index, train_data, val_data, variant):
 
     params['close_times'] = CLOSE_TIME_MAPPING[variant['code']]
     initial_amount = INIT_CASH_MAPPING[variant['code']]  #2000000.0  #60000
-    
+
     if params['check_freq'] == 0:
-        params['check_freq'] = params['step_len'] # 训练完一个周期评估一次
+        params['check_freq'] = params['step_len']  # 训练完一个周期评估一次
 
     total_timesteps = math.ceil(
         params['step_len'] * params['epchos'] / 10000) * 10000
 
-    pdb.set_trace()
-    tessla = Tessla(
-        code=variant['code'],
-        env_class=HedgeTraderEnv,
-        features=features,
-        state_space=state_space,
-        buy_cost_pct=buy_cost_pct_sets,
-        sell_cost_pct=sell_cost_pct_sets,
-        ticker_dim=ticker_dimension,
-        initial_amount=initial_amount,
-        cont_multnum=CONT_MULTNUM_MAPPING[variant['code']],
-        open_threshold=THRESHOLD_MAPPING[variant['code']]['long_open'],
-        close_threshold=THRESHOLD_MAPPING[variant['code']]['long_close'],
-        action_dim=2,
-        log_dir=g_log_path)
+    tessla = Tessla(code=variant['code'],
+                    env_class=HedgeTraderEnv,
+                    features=features,
+                    state_space=state_space,
+                    buy_cost_pct=buy_cost_pct_sets,
+                    sell_cost_pct=sell_cost_pct_sets,
+                    ticker_dim=ticker_dimension,
+                    initial_amount=initial_amount,
+                    direction=variant['direction'],
+                    cont_multnum=CONT_MULTNUM_MAPPING[variant['code']],
+                    action_dim=2,
+                    log_dir=g_log_path)
     tessla.train(name="{0}".format(index),
                  train_data=train_data,
                  val_data=val_data,
-                 g_tensorboard_path=g_tensorboard_path,
+                 tensorboard_path=g_tensorboard_path,
                  total_timesteps=total_timesteps,
                  train_path=g_train_path,
                  **params)
 
-def train(variant):
 
+def train(variant):
     data_mapping = load_datasets(variant)
     for i in range(variant['g_start_pos'],
                    variant['g_start_pos'] + variant['g_max_pos']):
         train_data, val_data, _ = data_mapping[i]
         fit(index=i, train_data=train_data, val_data=val_data, variant=variant)
+
+
+def evaluate(variant):
+    data_mapping = load_datasets(variant)
+    for i in range(variant['g_start_pos'],
+                   variant['g_start_pos'] + variant['g_max_pos']):
+        _, _, test_data = data_mapping[i]
+        educate(index=i,
+                test_data=test_data,
+                model_index='best_model',
+                variant=variant)
+
+
+def educate(index, test_data, model_index, variant):
+    features = [
+        col for col in test_data.columns
+        if col not in ['trade_time', 'code'] + ['price', 'close']
+    ]
+
+    ## 标的维度
+    ticker_dimension = len(test_data.code.unique())
+    state_space = ticker_dimension
+
+    ## 开仓手续费
+    buy_cost_pct = COST_MAPPING[variant['code']]['buy']
+    ## 平仓手续费
+    sell_cost_pct = COST_MAPPING[variant['code']][
+        'sell']  #0.00012  #0.1 / 1000  #0.000100#(0.0100 / 10000)
+
+    ## 手续费字典
+    buy_cost_pct_sets = dict(
+        zip(test_data.code, [buy_cost_pct] * ticker_dimension))
+    sell_cost_pct_sets = dict(
+        zip(test_data.code, [sell_cost_pct] * ticker_dimension))
+
+    params = copy.deepcopy(variant)
+    params['verbosity'] = 40
+    del params['direction']
+    params['direction'] = 1 if variant['direction'] == 'long' else -1
+    params['step_len'] = test_data.shape[0] - 1
+
+    params['close_times'] = CLOSE_TIME_MAPPING[variant['code']]
+    initial_amount = INIT_CASH_MAPPING[variant['code']]  #2000000.0  #60000
+
+    tessla = Tessla(code=variant['code'],
+                    env_class=HedgeTraderEnv,
+                    features=features,
+                    state_space=state_space,
+                    buy_cost_pct=buy_cost_pct_sets,
+                    sell_cost_pct=sell_cost_pct_sets,
+                    ticker_dim=ticker_dimension,
+                    initial_amount=initial_amount,
+                    direction=variant['direction'],
+                    cont_multnum=CONT_MULTNUM_MAPPING[variant['code']],
+                    action_dim=2,
+                    log_dir=g_log_path)
+
+    test_memory = tessla.evaluate(name="{0}".format(index),
+                                  test_data=test_data,
+                                  train_path=g_train_path,
+                                  **params)
+
+    tessla.illustrate(name="{0}".format(index),
+                      memory_data=test_memory[0],
+                      kl_pd=test_data,
+                      illustrate_path=g_illustrate_path,
+                      price_name='price',
+                      trader_time=TRADE_TIME_MAPPING[variant['code']],
+                      **params)
 
 
 if __name__ == '__main__':
@@ -138,7 +204,10 @@ if __name__ == '__main__':
     parser.add_argument('--direction', type=str, default='long')  ## 方向
     parser.add_argument('--code', type=str, default='RB')  ## 代码
     parser.add_argument('--g_instruments', type=str, default='rbb')  ## 标的
-    parser.add_argument('--param_index', type=int, default=1)  ## 参数索引
+
+    parser.add_argument('--param_index', type=int, default=1)  ## 模型参数索引
+    parser.add_argument('--addition_index', type=int, default=1)  ## 通用参数索引
+    parser.add_argument('--policy_key', type=str, default='neuro0004_1')  ## 策略参数索引
 
     parser.add_argument('--verbosity', type=int, default=40)  ## 训练时的输出频率
     parser.add_argument('--g_start_pos', type=int, default=47)
@@ -147,4 +216,6 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     variant = vars(args)
+
     train(variant=variant)
+    #evaluate(variant=variant)
