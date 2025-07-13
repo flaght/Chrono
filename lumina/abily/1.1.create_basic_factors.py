8cb0328bddd262ada517532de5efac7f189287c1
Kerry <flaght@gmail.com>
2025-07-13 00:47:51 +0000

    剥离合约环境变量

--- lumina/abily/1.1.create_basic_factors.py
+++ lumina/abily/1.1.create_basic_factors.py
@@ -7,12 +7,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-os.environ['INSTRUMENTS'] = 'ims'
-g_instruments = os.environ['INSTRUMENTS']
+#os.environ['INSTRUMENTS'] = 'ims'
+#g_instruments = os.environ['INSTRUMENTS']
 
 from alphacopilot.api.calendars import advanceDateByCalendar
 from kdutils.ttimes import get_dates
-from kdutils.macro import base_path, codes
+from kdutils.macro import base_path
+from config.contract import INSTRUMENTS_CODES
 from kdutils.data import fetch_main_market
 import lumina.env as env
 
@@ -33,7 +34,7 @@ import lumina.impulse.i012 as i012
 import lumina.impulse.i013 as i013
 
 
-def callback_save(factors_data, name, method, start_date, end_date):
+def callback_save(instruments, factors_data, name, method, start_date, end_date):
     cond1 = (factors_data.index.get_level_values(
         level=0) >= start_date) & (factors_data.index.get_level_values(
             level=0) <= (datetime.datetime.strptime(end_date, '%Y-%m-%d') +
@@ -41,17 +42,18 @@ def callback_save(factors_data, name, method, start_date, end_date):
     factors_data = factors_data[cond1]
     ff = factors_data.sort_index().reset_index()
     ff1 = ff  #ff.set_index(['trade_time', 'code']).unstack()
-    dirs = os.path.join(base_path, method, g_instruments, 'factors')
+    dirs = os.path.join(base_path, method, instruments, 'factors')
     if not os.path.exists(dirs):
         os.makedirs(dirs)
     filename = os.path.join(dirs,
                             '{0}_factors.feather'.format(name.split('.')[-1]))
+    print(filename)
     ff1.sort_index().reset_index(drop=True).to_feather(filename)
 
 
-def calculate_factors(data, callback, method, start_date, end_date):
+def calculate_factors(data, callback, instruments, method, start_date, end_date):
 
-    def run(data, i00, callback, method, start_date, end_date):
+    def run(data, i00, callback, instruments, method, start_date, end_date):
         res = []
         for f in i00.__all__:
             print(f)
@@ -64,46 +66,49 @@ def calculate_factors(data, callback, method, start_date, end_date):
             res.append(dt)
         data = pd.concat(res, axis=1)
         callback(factors_data=data,
+                 instruments=instruments,
                  name=i00.__name__,
                  method=method,
                  start_date=start_date,
                  end_date=end_date)
 
-    run(data, i001, callback, method, start_date, end_date)
-    run(data, i002, callback, method, start_date, end_date)
-    run(data, i003, callback, method, start_date, end_date)
-    run(data, i004, callback, method, start_date, end_date)
-    run(data, i005, callback, method, start_date, end_date)
-    run(data, i006, callback, method, start_date, end_date)
-    run(data, i007, callback, method, start_date, end_date)
-    run(data, i008, callback, method, start_date, end_date)
-    run(data, i009, callback, method, start_date, end_date)
-    run(data, i010, callback, method, start_date, end_date)
-    run(data, i011, callback, method, start_date, end_date)
-    run(data, i012, callback, method, start_date, end_date)
-    run(data, i013, callback, method, start_date, end_date)
+    for i00 in [
+            i001, i002, i003, i004, i005, i006, i007, i008, i009, i010, i011,
+            i012, i013
+    ]:
+        run(data=data,
+            i00=i00,
+            callback=callback,
+            instruments=instruments,
+            method=method,
+            start_date=start_date,
+            end_date=end_date)
 
 
-def main(method):
+def main(method, instruments):
+    pdb.set_trace()
     start_date, end_date = get_dates(method)
     start_time = advanceDateByCalendar('china.sse', start_date,
                                        '-{0}b'.format(1)).strftime('%Y-%m-%d')
     data = fetch_main_market(begin_date=start_time,
                              end_date=end_date,
-                             codes=codes)
+                             codes=[INSTRUMENTS_CODES[instruments]])
     data = data.set_index(['trade_time', 'code']).unstack()
     calculate_factors(data,
+                      instruments=instruments,
                       callback=callback_save,
                       method=method,
                       start_date=start_date,
                       end_date=end_date)
 
 
-def merge(method):
-    base_dirs = os.path.join(base_path, method, g_instruments, 'factors')
+def merge(method, instruments):
+    pdb.set_trace()
+    base_dirs = os.path.join(base_path, method, instruments, 'factors')
     res = []
     for root, dirs, files in os.walk(base_dirs):
         for file in files:
+            print(file)
             if file.endswith('.feather') and file != 'factors_data.feather':
                 factor_file = os.path.join(root, file)
                 factor_data = pd.read_feather(factor_file)
@@ -115,7 +120,8 @@ def merge(method):
     end_date = factors_data['trade_time'].max().strftime('%Y-%m-%d %H:%M:%S')
     data = fetch_main_market(begin_date=start_date,
                              end_date=end_date,
-                             codes=codes)
+                             codes=[INSTRUMENTS_CODES[instruments]])
+    pdb.set_trace()
     factors_data = factors_data.merge(data[[
         'trade_time', 'code', 'close', 'high', 'low', 'open', 'value',
         'volume', 'openint', 'vwap'
@@ -135,22 +141,26 @@ def merge(method):
     pdb.set_trace()
     train_data = factors_data[factors_data['trade_time'].isin(times[:len1])]
     val_data = factors_data[factors_data['trade_time'].isin(times[len1:len1 +
-                                                                len2])]
+                                                                  len2])]
     test_data = factors_data[factors_data['trade_time'].isin(times[len1 +
-                                                                 len2:])]
+                                                                   len2:])]
     ## 校验集
     ## 测试集
     ### 切割数据
     pdb.set_trace()
+    target_dir = os.path.join(base_path, method, instruments, 'basic')
+    if not os.path.exists(target_dir):
+        os.makedirs(target_dir)
     train_data.reset_index(drop=True).to_feather(
-        os.path.join(base_path, method, g_instruments, 'basic',
+        os.path.join(base_path, method, instruments, 'basic',
                      'train_data.feather'))
     val_data.reset_index(drop=True).to_feather(
-        os.path.join(base_path, method, g_instruments, 'basic',
+        os.path.join(base_path, method, instruments, 'basic',
                      'val_data.feather'))
     test_data.reset_index(drop=True).to_feather(
-        os.path.join(base_path, method, g_instruments, 'basic',
+        os.path.join(base_path, method, instruments, 'basic',
                      'test_data.feather'))
 
 
-merge('aicso2')
+main(method='aicso0', instruments='rbb')
+merge(method='aicso0', instruments='rbb')
