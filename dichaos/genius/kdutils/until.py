3948b760aff338f12a1f0dc4b26fdfdccc63ff4d
Kerry <flaght@gmail.com>
2025-08-11 05:20:29 +0000

    通用类

---
+++ dichaos/genius/kdutils/until.py
@@ -0,0 +1,7 @@
+import os
+def create_agent_path(name, method, symbol, date):
+    path = os.path.join(os.environ['BASE_PATH'], method, name, 'brain', symbol,
+                        date)
+    if not os.path.exists(path):
+        os.makedirs(path)
+    return path
