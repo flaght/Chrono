ade0e32f937b8cc37462d50970e3fd6ef6da3f12
Kerry <flaght@gmail.com>
2025-08-12 02:47:40 +0000

    修改测试列

--- dichaos/genius/apredict.py
+++ dichaos/genius/apredict.py
@@ -4,16 +4,15 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from alphacopilot.calendars.api import advanceDateByCalendar
-from agent import IndicatorPredict, CloutoPredict, PosFlowPredict
+from agent import IndicatorPredict, CloutoPredict, PosFlowPredict, MoneyFlowPredict
 
 
 def agent_predict1():
-    end_date = '2025-06-27'
+    end_date = '2025-02-14'
     begin_date = advanceDateByCalendar('china.sse', end_date,
                                        '-{0}b'.format(10)).strftime('%Y-%m-%d')
     symbol = 'IM'
-
-    predict = IndicatorPredict(symbol=symbol,
+    predict = CloutoPredict(symbol=symbol,
                                memory_path=os.path.join("records"),
                                date='2025-01-27')
 
@@ -22,11 +21,12 @@ def agent_predict1():
 
     response = asyncio.run(
         predict.agenerate_prediction(date=end_date, predict_data=predict_data))
+    print(response)
 
 
 async def agent_predict2(symbol):
     model_date = '2025-01-27'
-    end_date = '2025-06-27'
+    end_date = '2025-02-14'
     predict_sets = [
         PosFlowPredict(date=model_date,
                        memory_path=os.path.join("records"),
@@ -37,8 +37,10 @@ async def agent_predict2(symbol):
         IndicatorPredict(date=model_date,
                          memory_path=os.path.join("records"),
                          symbol=symbol),
+        MoneyFlowPredict(date=model_date,
+                         memory_path=os.path.join("records"),
+                         symbol=symbol)
     ]
-
 
     end_date = advanceDateByCalendar('china.sse', end_date,
                                      '-{0}b'.format(1)).strftime('%Y-%m-%d')
@@ -60,5 +62,5 @@ async def agent_predict2(symbol):
 
 
 if __name__ == "__main__":
-    #agent_predict1()
-    asyncio.run(agent_predict2(symbol='IM'))
+    agent_predict1()
+    #asyncio.run(agent_predict2(symbol='IM'))
