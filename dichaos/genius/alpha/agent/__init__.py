8ceb2aa2643250d60ffa2163dd67f770611eec3a
Kerry <flaght@gmail.com>
2025-08-15 14:10:01 +0000

    导入

--- dichaos/genius/alpha/agent/__init__.py
+++ dichaos/genius/alpha/agent/__init__.py
@@ -1,12 +1,14 @@
 from agent.indicator.predict import Predictor as IndicatorPredict
 from agent.moneyflow.predict import Predictor as MoneyFlowPredict
 from agent.clouto.predict import Predictor as CloutoPredict
+from agent.hotmoney.predict import Predictor as HotMoneyPredict
 
 from agent.indicator.train import Trainer as IndicatorTrain
 from agent.moneyflow.train import Trainer as MoneyFlowTrain
 from agent.clouto.train import Trainer as CloutoTrain
+from agent.hotmoney.train import Trainer as HotMoneyTrain
 
 __all__ = [
-    'IndicatorPredict', 'MoneyFlowPredict', 'CloutoPredict'
-    'IndicatorTrain', 'MoneyFlowTrain', 'CloutoTrain'
+    'IndicatorPredict', 'MoneyFlowPredict', 'CloutoPredict', 'HotMoneyPredict'
+    'IndicatorTrain', 'MoneyFlowTrain', 'CloutoTrain', 'HotMoneyTrain'
 ]
