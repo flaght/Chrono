9744c48a0bd5943958e6576086e43bfd80d4d701
Kerry <flaght@gmail.com>
2025-08-28 22:19:13 +0000

    客户端

---
+++ dichaos/genius/pulse/client.py
@@ -0,0 +1,61 @@
+import asyncio, pdb
+import sys, os, argparse
+from dotenv import load_dotenv
+
+load_dotenv()
+from dichaos.services.llm.factory import LLMServiceFactory
+from agents.chip_agent import ChipAnalysisAgent
+from agents.factor import IdeaAgent
+from agents.factor import ExpsAgent
+
+
+async def run_agent_workflow(agent, user_request: str, **kwargs):
+    """通用的 Agent 工作流执行器。"""
+    try:
+        pdb.set_trace()
+        await agent.connect_tool_server()
+        final_report = await agent.run(request=user_request, **kwargs)
+        print(final_report)
+    finally:
+        if agent:
+            await agent.cleanup()
+
+
+async def main():
+    llm_service = LLMServiceFactory.create_llm_service(
+        llm_model=os.environ['MODEL_NAME'],
+        llm_provider=os.environ['MODEL_PROVIDER'],
+        system_message='')
+
+    agent_map = {
+        'chip': (ChipAnalysisAgent, "分析股票 600519..."),
+        'idea': (IdeaAgent, "我观察到市场，发现量价共振因子，具备强预测性"),
+        "exps":
+        (ExpsAgent,
+         "因子名称：量价共振增强因子。计算方法：首先，计算过去N个交易日内每日的成交量变化率（当日成交量 - 前一日成交量）/ 前一日成交量，以及每日的价格变化率（当日收盘价 - 前一日收盘价）/ 前一日收盘价。然后，将每日的成交量变化率和价格变化率相乘，得到每日的量价协同指标."
+         )
+    }
+    agent_class, user_request = agent_map['idea']
+
+    agent_kwargs = {"llm_service": llm_service, "generate_final_report": False}
+
+    server_base_url = f"http://0.0.0.0:8001"
+    pdb.set_trace()
+    #temp_agent_for_config = agent_class(**agent_kwargs)
+    #agent_base_config = temp_agent_for_config.tool_server_config.copy()
+
+    #agent_base_config["base_url"] = server_base_url
+    agent_kwargs["tool_server_config"] = {
+        'transport': 'http',
+        #'module_path': 'tools.main_server',
+        'base_url': server_base_url
+    }
+
+    agent = await agent_class.create(**agent_kwargs)
+    workflow_params = {}
+
+    await run_agent_workflow(agent, user_request, **workflow_params)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
