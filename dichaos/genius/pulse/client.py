034a371bcd06d2aa250f93b44f9b67c7750f00f6
Kerry <flaght@gmail.com>
2025-09-01 13:20:03 +0000

    新增服务

--- dichaos/genius/pulse/client.py
+++ dichaos/genius/pulse/client.py
@@ -1,4 +1,4 @@
-import asyncio, pdb
+import asyncio, pdb, json
 import sys, os, argparse
 from dotenv import load_dotenv
 
@@ -7,6 +7,20 @@ from dichaos.services.llm.factory import LLMServiceFactory
 from agents.chip_agent import ChipAnalysisAgent
 from agents.factor import IdeaAgent
 from agents.factor import ExpsAgent
+from agents.factor import BuildAgent
+from agents.factor import Build1Agent
+from agents.factor import DSLAgent
+from agents.factor import ForgeAgent
+
+### 临时输入
+test_input = {
+    'money_flow_volume_in': '主动买入的总成交量',
+    'money_flow_volume_out': '主动卖出的总成交量',
+    'vwap_tick': '单笔成交均价',
+    'vwap_total': '时间窗⼝总成交均价',
+    'money_flow_net_volume_in': '净主动买⼊成交量',
+    'money_flow_smart_money_out': '聪明钱卖出⾦额（⾼于均价的主动卖单）'
+}
 
 
 async def run_agent_workflow(agent, user_request: str, **kwargs):
@@ -15,6 +29,7 @@ async def run_agent_workflow(agent, user_request: str, **kwargs):
         pdb.set_trace()
         await agent.connect_tool_server()
         final_report = await agent.run(request=user_request, **kwargs)
+        pdb.set_trace()
         print(final_report)
     finally:
         if agent:
@@ -33,9 +48,15 @@ async def main():
         "exps":
         (ExpsAgent,
          "因子名称：量价共振增强因子。计算方法：首先，计算过去N个交易日内每日的成交量变化率（当日成交量 - 前一日成交量）/ 前一日成交量，以及每日的价格变化率（当日收盘价 - 前一日收盘价）/ 前一日收盘价。然后，将每日的成交量变化率和价格变化率相乘，得到每日的量价协同指标."
-         )
+         ),
+        "build":
+        (BuildAgent, "我观察到市场，发现量价共振因子，具备强预测性. 包括了成交量,持仓量,主动买入总成交量,主动卖出总成交量,"),
+        "build1": (Build1Agent, "从工具集中推演因子"),
+        "dsl": (DSLAgent, "从工具集中推演因子"),
+        "forege": (ForgeAgent, json.dumps(test_input))
     }
-    agent_class, user_request = agent_map['idea']
+    agent_class, user_request = agent_map['build1']
+
 
     agent_kwargs = {"llm_service": llm_service, "generate_final_report": False}
 
@@ -50,7 +71,6 @@ async def main():
         #'module_path': 'tools.main_server',
         'base_url': server_base_url
     }
-
     agent = await agent_class.create(**agent_kwargs)
     workflow_params = {}
 
--- dichaos/genius/pulse/serve.py
+++ dichaos/genius/pulse/serve.py
@@ -9,12 +9,16 @@ from dichaos.services.mcp.registry import Registry
 from tools.chip_tool import ChipAnalysisTool
 from tools.factor import IdeaFactorTool
 from tools.factor import ExpsFactorTool
+from tools.factor import DSLFactorTool
+from tools.factor import ForgeFactorTool
 
 if __name__ == "__main__":
     registry = Registry()
     registry.add_tool(ChipAnalysisTool)
     registry.add_tool(IdeaFactorTool)
     registry.add_tool(ExpsFactorTool)
+    registry.add_tool(DSLFactorTool)
+    registry.add_tool(ForgeFactorTool)
     parser = argparse.ArgumentParser(description="Unified FastAPI Tool Server")
     parser.add_argument("--host", default="127.0.0.1")
     parser.add_argument("--port", type=int, default=8001)  # 使用一个统一的端口，比如 8000
