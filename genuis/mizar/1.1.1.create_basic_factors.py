df46531c4d5d3c82d3f4c133975eb69fe7e85180
Kerry <flaght@gmail.com>
2025-09-30 01:03:42 +0000

    调整参数

--- genuis/mizar/1.1.1.create_basic_factors.py
+++ genuis/mizar/1.1.1.create_basic_factors.py
@@ -262,6 +262,6 @@ def returns(method, instruments):
         os.path.join(target_dir, 'test_returns.feather'))
 
 
-main(method='cicso0', instruments='ims')
-merge(method='cicso0', instruments='ims')
+main(method='cicso0', instruments='ics')
+merge(method='cicso0', instruments='ics')
 #returns(method='cicso0', instruments='ics')
