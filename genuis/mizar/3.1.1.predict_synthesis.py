77618c4e62403f037e7053476d248689210c925c
Kerry <flaght@gmail.com>
2025-10-07 15:21:06 +0000

    新增合成方法

--- genuis/mizar/3.1.1.predict_synthesis.py
+++ genuis/mizar/3.1.1.predict_synthesis.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 load_dotenv()
 from kdutils.tactix import Tactix
 
-from lib.syn001 import build_factors, linear_train_model, lassocv_train_model
+from lib.syn001 import build_factors, linear_train_model, lassocv_train_model, rigde_train_model, lgb_train_model
 
 if __name__ == '__main__':
     variant = Tactix().start()
@@ -13,7 +13,7 @@ if __name__ == '__main__':
                       instruments=variant.instruments,
                       task_id=variant.task_id,
                       period=variant.period)
-    
+
     elif variant.form == 'linear':
         linear_train_model(method=variant.method,
                            instruments=variant.instruments,
@@ -25,3 +25,15 @@ if __name__ == '__main__':
                             instruments=variant.instruments,
                             task_id=variant.task_id,
                             period=variant.period)
+
+    elif variant.form == 'rigde':
+        rigde_train_model(method=variant.method,
+                          instruments=variant.instruments,
+                          task_id=variant.task_id,
+                          period=variant.period)
+
+    elif variant.form == 'lgb':
+        lgb_train_model(method=variant.method,
+                          instruments=variant.instruments,
+                          task_id=variant.task_id,
+                          period=variant.period)
