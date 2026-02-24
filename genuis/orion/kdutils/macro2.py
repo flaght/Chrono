4b0d121c506a5dbccb3395b3c9d7809fb80cb097
Kerry <flaght@gmail.com>
2026-02-24 01:43:16 +0000

    fixed marco

--- genuis/orion/kdutils/macro2.py
+++ genuis/orion/kdutils/macro2.py
@@ -4,3 +4,25 @@ base_path = os.path.join(os.environ['BASE_PATH'], os.environ['RECORD_PATH'])
 raw_path = os.path.join(os.environ['RAW_DATA_PATH'])
 
 FUTURES_MAP = {'um': 'futures', 'cm': 'futures', 'spot': 'spot'}
+
+
+
+
+PERIOD_MAPPING = {
+    "1d": "1day",
+    "1h": "1hour"
+}
+
+### 20260201  dx  ashare
+TASK_MAPPING = {
+    "20260201": {
+        "source": "ashare",
+        "period": "1d",
+        "cycle": "1h" ## 持仓1 horizon
+    },
+    "202060101": {
+        "source": "binance",
+        "period": "1h",
+        "cycle": "1h" ## 持仓1 horizon
+    }
+}
