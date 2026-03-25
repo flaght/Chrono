dddcdcae2f612723eae107b0449a8c3ba44f67cc
Kerry <flaght@gmail.com>
2026-03-25 08:33:24 +0000

    add params

--- genuis/mizar/lib/uvx.py
+++ genuis/mizar/lib/uvx.py
@@ -34,6 +34,50 @@ def fetch_clean_data2(method, task_id, instruments, output, params,
         final_data['trade_time'] <= time_array[test_time[-1]][-1])]
     return train_data, test_data
 
+def load_rl_params(file_dirs: str, trade_id: str, model_id: str, 
+                   feature_id:str, 
+                   env_id:str,
+                   train_id:str,
+                   regime_id:str,
+                   name:str = 'param'):
+    file_path = os.path.join(file_dirs, "params", "{}.yaml".format(name))
+    try:
+        with open(file_path, 'r', encoding='utf-8') as f:
+            config = yaml.safe_load(f)
+        config = config['params']
+        if 'trade' not in config or 'model' not in config:
+            raise KeyError("配置文件中必须包含 'params'顶级键。")
+        
+        if trade_id not in config['trade'] or model_id not in config['model']\
+            or feature_id not in config['feature'] or env_id not in config['env']\
+                or train_id not in config['train'] or regime_id not in config['env']:
+            raise KeyError(f"配置文件中没有 {trade_id} or {model_id} 数据")
+        
+        env_param = config['env'][env_id]
+        train_param = config['train'][train_id]
+        trade_param = config['trade'][trade_id]
+        model_param = config['model'][model_id]
+        feature_param = config['feature'][feature_id].split()
+        regime_min_param = config['regime'][regime_id]['min'].split()
+        regime_daily_param = config['regime'][regime_id]['daily'].split()
+        return env_param, trade_param, model_param, train_param, feature_param, \
+                    regime_min_param,regime_daily_param
+            
+
+    except FileNotFoundError:
+        print(f"错误：配置文件 '{file_path}' 不存在。")
+        return None, None
+    except yaml.YAMLError as e:
+        print(f"错误：解析YAML文件 '{file_path}' 失败: {e}")
+        return None, None
+    except KeyError as e:
+        print(f"错误：配置文件中缺少必需的键路径: {e}")
+        return None, None
+    except Exception as e:
+        print(f"发生未知错误: {e}")
+        return None, None
+    
+    
 def load_params2(file_dirs: str, name:str, model_name: str, data_name:str):
     file_path = os.path.join(file_dirs, "params", "{}.yaml".format(name))
     try:
