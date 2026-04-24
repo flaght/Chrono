d3bb8b68b12ca4fc1cfd1b8847c5a6dad9c86d50
Kerry <flaght@gmail.com>
2026-04-24 06:55:11 +0000

    add code

--- genuis/mizar/lib/flp001.py
+++ genuis/mizar/lib/flp001.py
@@ -3,6 +3,7 @@ from IPython.display import display, HTML
 from pathlib import Path
 import pandas as pd
 from kdutils.macro2 import base_path
+from lib.iux002 import create_id, generate_simple_id
 
 STANDARD_SCHEMA = {
     'name': None,
@@ -205,6 +206,36 @@ def load_data(method, task_id, instruments, period, session, category):
     results = pd.DataFrame(res)
     return results
 
+def load_data2(method, task_id, instruments, period):
+    res = []
+    file_path = os.path.join(base_path, method, instruments, 'rulex', task_id,
+                             "nxt1_ret_{0}h".format(period))
+    draft_data = pd.read_csv(os.path.join(file_path, "draft.csv"))
+    draft_data['source'] = draft_data['source'].astype(int)
+    draft_data['factor_id'] = draft_data['formula'].apply(lambda x: create_id(generate_simple_id(x)))
+    for row in draft_data.itertuples():
+        filename = os.path.join(file_path, "d{}".format(
+            row.source) if row.category=='d' else "{}".format(row.source),
+                                row.factor_id, "performance_summary.txt")
+        data1 = parse_summary(filename)
+        desired_order = [
+            'factor_id', 'formula', 'category', 'direction', 'source', 
+            'ic_mean', 'ann_sharpe', 'calmar', 'max_dd', 'avg_ret', 
+            'total_ret', 'win_rate', 'pl_ratio', 'turnover', 'factor_ac','plot']
+        
+        target_keys = ['avg_ret', 'ann_sharpe', 'max_dd', 'calmar', 
+                       'win_rate','pl_ratio','ic_mean', 'turnover',
+                       'factor_ac','total_ret']
+        extracted_data = {k: data1.get(k) for k in target_keys}
+        extracted_data.update(row._asdict())
+        extracted_data["plot"] = os.path.join(file_path, "d{}".format(
+            row.source) if row.category=='d' else "{}".format(row.source),
+                                row.factor_id, "evaluation_plot.png")
+        ordered_data = {k: extracted_data.get(k) for k in desired_order}
+        res.append(ordered_data)
+    return pd.DataFrame(res)
+    
+    
 def make_clickable(val):
     return f'<a target="_blank" href="{val}">{val}</a>'
 
