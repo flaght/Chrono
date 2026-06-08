f998fb51d56eb17f599481b4a77321562b739201
Kerry <flaght@gmail.com>
2026-06-08 07:26:20 +0000

    线性合成

---
+++ genuis/mizar/3.1.4.linear_synthesis.py
@@ -0,0 +1,90 @@
+import os, json, pdb, copy
+import pandas as pd
+from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from kdutils.macro2 import *
+from kdutils.tactix import Tactix
+from lib.syn001.linear import composit_equal
+
+
+def load_data2(method, instruments, task_id, period):
+    base_dirs = os.path.join(base_path, method, instruments, 'temp', 'model',
+                             str(task_id), str(period), 'rl', 'data')
+
+    train_data = pd.read_feather(os.path.join(base_dirs, "train_data.feather"))
+
+    val_data = pd.read_feather(os.path.join(base_dirs, "val_data.feather"))
+
+    test_data = pd.read_feather(os.path.join(base_dirs, "test_data.feather"))
+    return train_data, val_data, test_data
+
+
+def equal_weight(train_data, val_data, test_data, corr_data, corr, period):
+
+    train_result, train_evaluate1 = composit_equal(
+        data=train_data,
+        selected_features=corr_data['expression'].to_list(),
+        roll_win=15,
+        period=period,
+        scale_method='raw',
+        expression='corr_{0}'.format(corr))
+
+    val_result, val_evaluate1 = composit_equal(
+        data=val_data,
+        selected_features=corr_data['expression'].to_list(),
+        roll_win=15,
+        period=period,
+        scale_method='raw',
+        expression='corr_{0}'.format(corr))
+
+    test_result, test_evaluate1 = composit_equal(
+        data=test_data,
+        selected_features=corr_data['expression'].to_list(),
+        roll_win=15,
+        period=period,
+        scale_method='raw',
+        expression='corr_{0}'.format(corr))
+
+    pdb.set_trace()
+    print('-->')
+
+
+def train_model(method, instruments, task_id, period):
+    train_data, val_data, test_data = load_data2(method=method,
+                                                 instruments=instruments,
+                                                 task_id=task_id,
+                                                 period=period)
+
+    dt_path = os.path.join(base_path, method, instruments, 'temp', 'model',
+                           str(task_id), str(period), 'rl', 'blend', 'corr')
+
+    pdb.set_trace()
+    file_path = Path(dt_path)
+    for csv_file in file_path.rglob('*.csv'):
+        pdb.set_trace()
+        corr_data = pd.read_csv(csv_file, index_col=0)
+        name = csv_file.parts[-1].split('.')[0]
+        train_data1 = train_data[['trade_time', 'code'] +
+                                 corr_data['expression'].to_list()]
+        val_data1 = val_data[['trade_time', 'code'] +
+                             corr_data['expression'].to_list()]
+        test_data1 = test_data[['trade_time', 'code'] +
+                               corr_data['expression'].to_list()]
+
+        equal_weight(train_data=train_data,
+                     val_data=val_data,
+                     test_data=test_data,
+                     corr_data=corr_data,
+                     corr=name,
+                     period=period)
+
+
+if __name__ == '__main__':
+    variant = Tactix().start()
+    train_model(method=variant.method,
+                instruments=variant.instruments,
+                task_id=variant.task_id,
+                period=variant.period)
