7af99405552eeb3e2c4c291432e54f2632c42bcb
Kerry <flaght@gmail.com>
2026-06-24 00:59:57 +0000

    fixed backtest

--- genuis/mizar/3.1.5.linear_signal_backtest.py
+++ genuis/mizar/3.1.5.linear_signal_backtest.py
@@ -3,121 +3,133 @@ import pandas as pd
 from joblib import Parallel, delayed
 from pathlib import Path
 from dotenv import load_dotenv
+import multiprocessing as mp
 
 load_dotenv()
 
+from lib.uvx import *
 from lib.cux001 import FactorEvaluate1
 from lib.rl012.sandbox import PositionBacktester
 from kdutils.macro2 import *
 from kdutils.tactix import Tactix
 from lib.attr001.ftd001 import *
-from lib.bck001 import *
+#from lib.bck001 import *
+from lib.bck002 import *
+
+# signal_functions = {
+#     "band_signal": {
+#         "1001": {
+#             "roll_num": 0,
+#             "threshold": 0.05,
+#         },
+#         "1002": {
+#             "roll_num": 0,
+#             "threshold": 0.10,
+#         },
+#         "1003": {
+#             "roll_num": 0,
+#             "threshold": 0.15,
+#         },
+#         "1004": {
+#             "roll_num": 0,
+#             "threshold": 0.20,
+#         },
+#     },
+#     "gate_signal": {
+#         "1001": {
+#             "roll_num": 20,
+#             "threshold": 0.75,
+#         },
+#         "1002": {
+#             "roll_num": 24,
+#             "threshold": 0.80,
+#         },
+#         "1003": {
+#             "roll_num": 30,
+#             "threshold": 0.85,
+#         },
+#         "1004": {
+#             "roll_num": 40,
+#             "threshold": 0.90,
+#         },
+#     },
+#     "rollrank1_signal": {
+#         "1001": {
+#             "roll_num": 20,
+#             "threshold": 0.80,
+#         },
+#         "1002": {
+#             "roll_num": 24,
+#             "threshold": 0.85,
+#         },
+#         "1003": {
+#             "roll_num": 30,
+#             "threshold": 0.90,
+#         },
+#     },
+#     "rollrank2_signal": {
+#         "1001": {
+#             "roll_num": 20,
+#             "threshold": 0.75,
+#         },
+#         "1002": {
+#             "roll_num": 24,
+#             "threshold": 0.80,
+#         },
+#         "1003": {
+#             "roll_num": 30,
+#             "threshold": 0.85,
+#         },
+#     }
+# }
 
 signal_functions = {
-    "band_signal": {
-        "1001": {
-            "roll_num": 0,
-            "threshold": 0.05,
-        },
-        "1002": {
-            "roll_num": 0,
-            "threshold": 0.10,
-        },
-        "1003": {
-            "roll_num": 0,
-            "threshold": 0.15,
-        },
-        "1004": {
-            "roll_num": 0,
-            "threshold": 0.20,
-        },
-    },
-    "gate_signal": {
-        "1001": {
-            "roll_num": 20,
-            "threshold": 0.75,
-        },
-        "1002": {
-            "roll_num": 24,
-            "threshold": 0.80,
-        },
-        "1003": {
-            "roll_num": 30,
-            "threshold": 0.85,
-        },
-        "1004": {
-            "roll_num": 40,
-            "threshold": 0.90,
-        },
-    },
-    "rollrank1_signal": {
-        "1001": {
-            "roll_num": 20,
-            "threshold": 0.80,
-        },
-        "1002": {
-            "roll_num": 24,
-            "threshold": 0.85,
-        },
-        "1003": {
-            "roll_num": 30,
-            "threshold": 0.90,
-        },
-    },
     "rollrank2_signal": {
-        "1001": {
-            "roll_num": 20,
-            "threshold": 0.75,
-        },
         "1002": {
             "roll_num": 24,
             "threshold": 0.80,
-        },
-        "1003": {
-            "roll_num": 30,
-            "threshold": 0.85,
-        },
+        }
     }
 }
 
 _PARALLEL_MARKET_DATA = None
 
 
-def parallel_backtest(signal_data, name, expression, code, period,
-                      max_position, lot_per_signal, contract_multiplier):
-    pdb.set_trace()
+def parallel_backtest(signal_data, name, parts, period, contract_multiplier,
+                      params, basic_path):
+    print(name)
     signal_data = signal_data.rename(columns={'transformed': 'value'})
     market_data = _PARALLEL_MARKET_DATA
-    # position_data = build_locked_signals(model_output=signal_data,
-    #                                      base_position=max_position,
-    #                                      lot_per_signal=lot_per_signal,
-    #                                      cooldown_bars=0,
-    #                                      hold_bars=period,
-    #                                      entry_resampling_win=period,
-    #                                      date_col='trade_date'
-    #                                      if 'trade_date' in signal_data.columns
-    #                                      else None)
     position_data = build_capped_locked_signals(
         model_output=signal_data,
-        base_position=max_position,
-        lot_per_signal=lot_per_signal,
+        base_position=params['base_position'],
+        lot_per_signal=params['lot_per_signal'],
         cooldown_bars=0,
         hold_bars=period,
-        entry_resampling_win=period,
+        entry_resampling_win=params['entry_resampling_win'],
         date_col='trade_date' if 'trade_date' in signal_data.columns else None,
-        max_daily_open_lots=10,
-        max_daily_open_lots_per_direction=5,
-        max_active_open_lots=2,
-        max_active_open_lots_per_direction=1,
-        min_abs_value=None,
-        block_same_direction_reentry=True,
-        block_opposite_direction_reentry=False,
-    )
+        max_daily_open_lots=params['max_daily_open_lots'],  #10,
+        max_daily_open_lots_per_direction=params[
+            'max_daily_open_lots_per_direction'],  #5,
+        max_active_open_lots=params['max_active_open_lots'],  #2,
+        max_active_open_lots_per_direction=params[
+            'max_active_open_lots_per_direction'],  #1,
+        extend_same_direction=params['extend_same_direction'],
+        min_abs_value=params['min_abs_value'],
+        block_same_direction_reentry=params['block_same_direction_reentry'],
+        block_opposite_direction_reentry=params[
+            'block_opposite_direction_reentry'])
+
     pb = PositionBacktester(market_data=market_data,
                             contract_multiplier=contract_multiplier,
                             slippage=0.001)
     trade_records, daily_stats = pb.run(position_df=position_data, code='RB')
-    print('-->')
+    dirs1 = os.path.join(basic_path, parts[-4], parts[-3], parts[-2], name)
+    os.makedirs(dirs1, exist_ok=True)
+    pdb.set_trace()
+    position_data.to_feather(os.path.join(dirs1, "position_data.feather"))
+    trade_records.to_feather(os.path.join(dirs1, "trade_records.feather"))
+    daily_stats.to_feather(os.path.join(dirs1, "daily_stats.feather"))
 
 
 def backtest_signal1(method, instruments, task_id, period, corr):
@@ -137,16 +149,14 @@ def backtest_signal1(method, instruments, task_id, period, corr):
         name = feat_file.parts[-1].split('.')[0]
         if 'test' not in name:
             continue
-        expression = "{0}_{1}_{2}_{3}".format(feat_file.parts[-4],
-                                              feat_file.parts[-3],
-                                              feat_file.parts[-2], name)
+        parts = feat_file.parts
         min_time = signal_data['trade_time'].min(
         ) if min_time is None else min(signal_data['trade_time'].min(),
                                        min_time)
         max_time = signal_data['trade_time'].max(
         ) if max_time is None else max(signal_data['trade_time'].max(),
                                        max_time)
-        res.append((name, expression, signal_data))
+        res.append((name, parts, signal_data))
 
     market_data = load_market_data(instruments=instruments,
                                    begin_time=min_time,
@@ -155,14 +165,73 @@ def backtest_signal1(method, instruments, task_id, period, corr):
     global _PARALLEL_MARKET_DATA
     _PARALLEL_MARKET_DATA = market_data
     pdb.set_trace()
-    parallel_backtest(signal_data=res[11][2],
-                      name=res[11][0],
-                      expression=res[11][1],
-                      code='RB',
-                      period=period,
-                      max_position=1,
-                      lot_per_signal=1,
-                      contract_multiplier=10)
+
+    params = {
+        'base_position': 10,
+        'lot_per_signal': 1,
+        'entry_resampling_win': period,
+        'max_daily_open_lots': 6,  #每个交易日总共最多接受多少手“开暴露”信号
+        'max_daily_open_lots_per_direction': 3,  # 每个交易日每个方向最多接受多少手
+        'max_active_open_lots': 1,  #  同一时刻最多允许多少手暴露尚未恢复
+        'max_active_open_lots_per_direction': 1,  # 同一时刻每个方向最多允许多少手暴露尚未恢复
+        'min_abs_value': None,
+        'block_same_direction_reentry': True,  # 同方向暴露未恢复前，不再接受同方向新信号。
+        'block_opposite_direction_reentry': True,  # 任一反方向暴露未恢复前，不接受当前方向新信号。
+        'extend_same_direction': True  # 若暴露到期 bar 又出现同方向信号，
+    }
+
+    params = {
+        "base_position": 100000,
+        "lot_per_signal": 1,
+        "entry_resampling_win": None,
+        "max_daily_open_lots": None,
+        "max_daily_open_lots_per_direction": None,
+        "max_active_open_lots": None,
+        "max_active_open_lots_per_direction": None,
+        "min_abs_value": None,
+        "block_same_direction_reentry": False,
+        "block_opposite_direction_reentry": False,
+        "extend_same_direction": False,
+    }
+
+    basic_path = os.path.join(base_path, method, instruments, 'temp', 'model',
+                              str(task_id), str(period), 'rl', 'backtest',
+                              'equal_weight', Params.create_tag(params))
+
+    os.makedirs(basic_path, exist_ok=True)
+    config_path = os.path.join(basic_path, "config.json")
+    with open(config_path, "w", encoding="utf-8") as f:
+        json.dump(params, f, indent=2, ensure_ascii=False)
+
+    args_for_backtest = [(item[2], item[0], item[1], period, 10, params,
+                          basic_path) for item in res]
+
+    if len(res) <= 1:
+        res = [
+            parallel_backtest(signal_data=args_for_backtest[0][0],
+                              name=args_for_backtest[0][1],
+                              parts=args_for_backtest[0][2],
+                              period=args_for_backtest[0][3],
+                              contract_multiplier=args_for_backtest[0][4],
+                              params=args_for_backtest[0][5],
+                              basic_path=args_for_backtest[0][6])
+        ]
+    else:
+        try:
+            ctx = mp.get_context("fork")
+        except ValueError:
+            ctx = mp.get_context()
+        with ctx.Pool(processes=4) as pool:
+            res = pool.starmap(parallel_backtest, args_for_backtest)
+    print(Params.create_tag(params))
+    # parallel_backtest(signal_data=res[11][2],
+    #                   name=res[11][0],
+    #                   expression=res[11][1],
+    #                   code='RB',
+    #                   period=period,
+    #                   max_position=1,
+    #                   lot_per_signal=1,
+    #                   contract_multiplier=10)
 
 
 def metrics_signal1(method, instruments, task_id, period, corr):
@@ -250,7 +319,7 @@ def create_signal1(method, instruments, task_id, period, corr):
     #                         contract_multiplier=10,
     #                         slippage=0.1)
     # trade_records, daily_stats = pb.run(position_df=position_data, code='RB')
-    print('-->')
+    # print('-->')
 
 
 if __name__ == '__main__':
