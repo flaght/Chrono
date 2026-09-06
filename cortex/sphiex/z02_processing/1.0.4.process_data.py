806bf3325bcf42cb131649c657ec035dbf62f80a
Kerry <flaght@gmail.com>
2026-09-06 01:59:40 +0000

    数据处理

--- cortex/sphiex/z02_processing/1.0.4.process_data.py
+++ cortex/sphiex/z02_processing/1.0.4.process_data.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 from kdutils.macro import base_path
-from kdutils.ttimes import get_dates
+from kdutils.ttimes import get_dates, get_ranges
 from lib.svx001 import scale_factors
 from macro import REGIME_FEATURES, PREDICT_FEATURES, TEXT_FEATURES
 
@@ -16,19 +16,19 @@ def process_regime_data(method):
     base_dir = os.path.join(base_path, "basic", method)
     regime_data = pd.read_feather(os.path.join(base_dir,
                                                "regime_data.feather"))
-    factor_names = [
-        f for f in regime_data.columns
-        if f not in ['trade_date', 'code', 'nxt1_ret_1h']
-    ]
-    for f in factor_names:
-        print(f)
-        scale_factors(
-            factor_data=regime_data,
-            method='roll_zscore',
-            win=5,  # 放在环境变量里，原始数据扩展
-            factor_name=f)
-        regime_data[f] = regime_data['transformed']
-        regime_data.drop(['transformed'], axis=1, inplace=True)
+    # factor_names = [
+    #     f for f in regime_data.columns
+    #     if f not in ['trade_date', 'code', 'nxt1_ret_1h']
+    # ]
+    # for f in factor_names:
+    #     print(f)
+    #     scale_factors(
+    #         factor_data=regime_data,
+    #         method='roll_zscore',
+    #         win=5,  # 放在环境变量里，原始数据扩展
+    #         factor_name=f)
+    #     regime_data[f] = regime_data['transformed']
+    #     regime_data.drop(['transformed'], axis=1, inplace=True)
     regime_data = regime_data[(regime_data['trade_date'] >= begin_date)
                               & (regime_data['trade_date'] <= end_date)]
     regime_data = regime_data.dropna().reset_index(drop=True)
@@ -50,7 +50,9 @@ def process_textuals(method):
     event_data = event_data.rename(columns={
         'date': 'trade_date'
     }).reset_index(drop=True)
-    return event_data[event_data['feature_type'].isin(TEXT_FEATURES)]
+    event_data['trade_date'] = pd.to_datetime(event_data['trade_date'])
+    return event_data[event_data['feature_type'].isin(
+        TEXT_FEATURES)].reset_index(drop=True)
 
 
 def process_predict_data(method):
@@ -89,17 +91,81 @@ def process_returns(method, period):
     return return_data
 
 
+def splite_data(data, train_range, val_range, test_range):
+    train_data = data[(data['trade_date'] >= train_range[0])
+                      & (data['trade_date'] <= train_range[1])]
+    val_data = data[(data['trade_date'] >= val_range[0])
+                    & (data['trade_date'] <= val_range[1])]
+
+    test_data = data[(data['trade_date'] >= test_range[0])
+                     & (data['trade_date'] <= test_range[1])]
+    return train_data.reset_index(drop=True), val_data.reset_index(
+        drop=True), test_data.reset_index(drop=True)
+
+
 ## 输出切割
 if __name__ == '__main__':
-    method = 'train0'
+    method = 'test0'
     period = 3
     regime_data = process_regime_data(method=method)
     predict_data = process_predict_data(method=method)
     textuals_data = process_textuals(method=method)
     returns_data = process_returns(method=method, period=period)
-    pdb.set_trace()
+    
+    # aligned_data = pd.merge(returns_data,textuals_data,on='trade_date',how='left') 主要用于检查 textuals_data是否有缺收
     output_dir = os.path.join(base_path, "normal", method)
     os.makedirs(output_dir, exist_ok=True)
+    train_range, val_range, test_range = get_ranges(method=method)
+    train_regime_data, val_regime_data, test_regime_data = splite_data(
+        data=regime_data,
+        train_range=train_range,
+        val_range=val_range,
+        test_range=test_range)
+
+    train_predict_data, val_predict_data, test_predict_data = splite_data(
+        data=predict_data,
+        train_range=train_range,
+        val_range=val_range,
+        test_range=test_range)
+
+    train_textuals_data, val_textuals_data, test_textuals_data = splite_data(
+        data=textuals_data,
+        train_range=train_range,
+        val_range=val_range,
+        test_range=test_range)
+
+    train_returns_data, val_returns_data, test_returns_data = splite_data(
+        data=returns_data,
+        train_range=train_range,
+        val_range=val_range,
+        test_range=test_range)
+    pdb.set_trace()
+    train_regime_data.to_feather(
+        os.path.join(output_dir, "train_regime_data.feather"))
+    train_predict_data.to_feather(
+        os.path.join(output_dir, "train_predict_data.feather"))
+    train_textuals_data.to_feather(
+        os.path.join(output_dir, "train_textuals_data.feather"))
+    train_returns_data.to_feather(
+        os.path.join(output_dir, "train_returns_data.feather"))
+
+    val_regime_data.to_feather(
+        os.path.join(output_dir, "val_regime_data.feather"))
+    val_predict_data.to_feather(
+        os.path.join(output_dir, "val_predict_data.feather"))
+    val_textuals_data.to_feather(
+        os.path.join(output_dir, "val_textuals_data.feather"))
+    val_returns_data.to_feather(
+        os.path.join(output_dir, "val_returns_data.feather"))
+
+    test_regime_data.to_feather(
+        os.path.join(output_dir, "test_regime_data.feather"))
+    test_predict_data.to_feather(
+        os.path.join(output_dir, "test_predict_data.feather"))
+    test_textuals_data.to_feather(
+        os.path.join(output_dir, "test_textuals_data.feather"))
+    test_returns_data.to_feather(
+        os.path.join(output_dir, "test_returns_data.feather"))
 
     regime_data.to_feather(os.path.join(output_dir, "regime_data.feather"))
     predict_data.to_feather(os.path.join(output_dir, "predict_data.feather"))
