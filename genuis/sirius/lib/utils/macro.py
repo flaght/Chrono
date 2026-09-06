5be87569743a3a1d02de5ac9f6dc064d2c42e17a
Kerry <flaght@gmail.com>
2026-09-06 14:52:06 +0000

    add utils

---
+++ genuis/sirius/lib/utils/macro.py
@@ -0,0 +1,2 @@
+import os
+base_path = os.path.join(os.environ['BASE_PATH'], 'records')
---
+++ genuis/sirius/lib/utils/ttimes.py
@@ -0,0 +1,16 @@
+def get_dates(method):
+    if method == 'bisco1':
+        return '2012-01-01', '2018-01-01'
+    if method == 'ricso2':
+        return '2012-01-01', '2026-04-30'
+    elif method == 'ricso3':
+        return '2025-03-01', '2026-04-30'
+
+
+FIIXED_MAPPING = {
+    'ricso2': {
+        'train_end': '2022-04-30 15:00:00',
+        'val_end': '2025-04-30 21:00:00',
+        'recent_start': '2021-03-25 21:00:00'
+    }
+}
